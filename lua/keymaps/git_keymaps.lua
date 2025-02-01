62d539abfda324487e2ccfbfe8a39a4b40a08fe8
ChrisRistoff <doanusho@gmail.com>
2025-02-01 20:13:39 +0000

    add git keymap to keep both local and remote

--- lua/keymaps/git_keymaps.lua
+++ lua/keymaps/git_keymaps.lua
@@ -6,6 +6,7 @@ vim.keymap.set("n", "<leader>gd", ":Gvdiffsplit!<CR>", { desc = "Three way diff 
 
 vim.keymap.set("n", "<leader>gl", "2do", { desc = "Keep LOCAL version (Your changes)" })  -- Local
 vim.keymap.set("n", "<leader>gr", "4do", { desc = "Keep REMOTE version (Their changes)" })  -- Remote
+vim.keymap.set("n", "<leader>gb", "2do:diffget 4<CR>", { desc = "Keep BOTH changes (Local + Remote)" })  -- Both
 
 vim.keymap.set("n", "<leader>gn", "]c", { desc = "Next Git conflict" })  -- Move to next conflict
 vim.keymap.set("n", "<leader>gp", "[c", { desc = "Previous Git conflict" })  -- Move to previous conflict
