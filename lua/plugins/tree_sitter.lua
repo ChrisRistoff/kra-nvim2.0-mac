3d2290a02757c0d1e3f089dfd1c8517d2711c719
ChrisRistoff <doanusho@gmail.com>
2026-04-18 12:13:41 +0100

    tree sitter config update

--- lua/plugins/tree_sitter.lua
+++ lua/plugins/tree_sitter.lua
@@ -3,68 +3,94 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
-        dependencies = {
-            "nvim-treesitter/nvim-treesitter-textobjects",
-        },
+        branch = "main",
+        build = ":TSUpdate",
+        lazy = false,
         config = function()
-            require("nvim-treesitter.configs").setup({
-                ensure_installed = { "c", "cpp", "php", "javascript", "typescript", "lua", "python", "vim", "html", "css", "json", "yaml", "prisma", "sql", "c_sharp", "markdown", "markdown_inline" },
-                auto_install = true,
-                highlight = { enable = true },
-                indent = { enable = true, disable = { "python", "typescript", "javascript", "tsx", "jsx" } },
-                incremental_selection = {
-                    enable = true,
-                    keymaps = {
-                        init_selection = "<c-space>",
-                        node_incremental = "<c-space>",
-                        scope_incremental = "<c-s>",
-                        node_decremental = "<M-space>",
-                    },
+            -- setup() is optional; only needed to override install_dir
+            require("nvim-treesitter").setup()
+
+            -- Install parsers (async; use :wait() only in headless/bootstrap scripts)
+            require("nvim-treesitter").install({
+                "c", "cpp", "php", "javascript", "typescript", "lua", "python",
+                "vim", "html", "css", "json", "yaml", "prisma", "sql", "c_sharp",
+                "markdown", "markdown_inline",
+            })
+
+            -- Highlighting is now provided by Neovim itself via vim.treesitter.start()
+            -- Indentation is provided by nvim-treesitter via the indentexpr below
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = {
+                    "c", "cpp", "php", "javascript", "typescript", "lua", "python",
+                    "vim", "html", "css", "json", "yaml", "prisma", "sql", "cs",
+                    "markdown",
                 },
-                textobjects = {
-                    select = {
-                        enable = true,
-                        lookahead = true,
-                        keymaps = {
-                            ["aa"] = "@parameter.outer",
-                            ["ia"] = "@parameter.inner",
-                            ["af"] = "@function.outer",
-                            ["if"] = "@function.inner",
-                            ["ac"] = "@class.outer",
-                            ["ic"] = "@class.inner",
-                        },
+                callback = function()
+                    -- Enable treesitter highlighting
+                    vim.treesitter.start()
+
+                    -- Enable treesitter-based folding
+                    vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+                    vim.wo[0][0].foldmethod = "expr"
+                    vim.wo[0][0].foldlevel = 99  -- start fully expanded
+
+                    -- Enable treesitter indentation (experimental) for languages
+                    -- where it works well; omit ones that were in your old disable list
+                    local ft = vim.bo.filetype
+                    local indent_disabled = { python = true, typescript = true, javascript = true }
+                    if not indent_disabled[ft] then
+                        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                    end
+                end,
+            })
+        end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        branch = "main",
+        lazy = false,
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        config = function()
+            require("nvim-treesitter-textobjects").setup({
+                select = {
+                    lookahead = true,
+                    selection_modes = {
+                        ["@parameter.outer"] = "v",
+                        ["@function.outer"]  = "V",
+                        ["@class.outer"]     = "V",
                     },
-                    move = {
-                        enable = true,
-                        set_jumps = true,
-                        goto_next_start = {
-                            ["]m"] = "@function.outer",
-                            ["]]"] = "@class.outer",
-                        },
-                        goto_next_end = {
-                            ["]M"] = "@function.outer",
-                            ["]["] = "@class.outer",
-                        },
-                        goto_previous_start = {
-                            ["[m"] = "@function.outer",
-                            ["[["] = "@class.outer",
-                        },
-                        goto_previous_end = {
-                            ["[M"] = "@function.outer",
-                            ["[]"] = "@class.outer",
-                        },
-                    },
-                    swap = {
-                        enable = true,
-                        swap_next = {
-                            ["<leader>a"] = "@parameter.inner",
-                        },
-                        swap_previous = {
-                            ["<leader>A"] = "@parameter.inner",
-                        },
-                    },
+                    include_surrounding_whitespace = false,
+                },
+                move = {
+                    set_jumps = true,
                 },
             })
-        end
-    }
+
+            local select = require("nvim-treesitter-textobjects.select")
+            local move   = require("nvim-treesitter-textobjects.move")
+            local swap   = require("nvim-treesitter-textobjects.swap")
+
+            -- ── Select text objects ──────────────────────────────────────────
+            vim.keymap.set({ "x", "o" }, "aa", function() select.select_textobject("@parameter.outer", "textobjects") end)
+            vim.keymap.set({ "x", "o" }, "ia", function() select.select_textobject("@parameter.inner", "textobjects") end)
+            vim.keymap.set({ "x", "o" }, "af", function() select.select_textobject("@function.outer",  "textobjects") end)
+            vim.keymap.set({ "x", "o" }, "if", function() select.select_textobject("@function.inner",  "textobjects") end)
+            vim.keymap.set({ "x", "o" }, "ac", function() select.select_textobject("@class.outer",     "textobjects") end)
+            vim.keymap.set({ "x", "o" }, "ic", function() select.select_textobject("@class.inner",     "textobjects") end)
+
+            -- ── Move between text objects ────────────────────���───────────────
+            vim.keymap.set({ "n", "x", "o" }, "]m", function() move.goto_next_start("@function.outer",  "textobjects") end)
+            vim.keymap.set({ "n", "x", "o" }, "]]", function() move.goto_next_start("@class.outer",     "textobjects") end)
+            vim.keymap.set({ "n", "x", "o" }, "]M", function() move.goto_next_end("@function.outer",    "textobjects") end)
+            vim.keymap.set({ "n", "x", "o" }, "][", function() move.goto_next_end("@class.outer",       "textobjects") end)
+            vim.keymap.set({ "n", "x", "o" }, "[m", function() move.goto_previous_start("@function.outer", "textobjects") end)
+            vim.keymap.set({ "n", "x", "o" }, "[[", function() move.goto_previous_start("@class.outer",    "textobjects") end)
+            vim.keymap.set({ "n", "x", "o" }, "[M", function() move.goto_previous_end("@function.outer",   "textobjects") end)
+            vim.keymap.set({ "n", "x", "o" }, "[]", function() move.goto_previous_end("@class.outer",      "textobjects") end)
+
+            -- ── Swap parameters ──────────────────────────────────────────────
+            vim.keymap.set("n", "<leader>a", function() swap.swap_next("@parameter.inner")     end)
+            vim.keymap.set("n", "<leader>A", function() swap.swap_previous("@parameter.inner") end)
+        end,
+    },
 }
